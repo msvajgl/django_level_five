ae1caaaa9b2ce0cda617860079d97898a6f4ed44
msvajgl <msvajgl@gmail.com>
2021-08-06 13:30:12 +0200

    lekce 161

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from . import models
 # from django.http import HttpResponse
 
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+class SchooListView(ListView):
+    model = models.School
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'BASIC INJECTION!'
-        return context
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['injectme'] = 'BASIC INJECTION!'
+#         return context
 
 
 # class CBView(View):
